a4983c32fba7feedab7600e9e7864a09c13ce726
Le-Li <htts270910@gmail.com>
2017-01-29 10:06:37 +0100

    Add the exercise 2 in PractiquePython.py

--- PractiquePython.py
+++ PractiquePython.py
@@ -3,4 +3,11 @@
 print"%s, you will turn 100 in %s" %(raw_input("What is your name? "),
 2116-int(raw_input("What is your age? ")))
 
+#exercise 2: Odd or Even 
+##########################################################################
+# extra 2
+num=int(raw_input("Enter a number to check (num): "))
+check=int(raw_input("Enter a number to divide by (check): "))
+if num % check == 0: print(str(check)+" divides evenly into "+str(num)) 
+else: print(str(check)+" does not divide evenly into "+str(num))
 
