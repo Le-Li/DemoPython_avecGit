6f1f60be172759fba24ef6dacce242e8bee19e2e
Le-Li <htts270910@gmail.com>
2017-01-29 10:00:10 +0100

    Create a new file PractiquePython.py with the exercise 1

---
+++ PractiquePython.py
@@ -0,0 +1,6 @@
+#exercise 1: Character Input
+##########################################################################
+print"%s, you will turn 100 in %s" %(raw_input("What is your name? "),
+2116-int(raw_input("What is your age? ")))
+
+
